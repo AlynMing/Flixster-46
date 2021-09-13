d82c337b4422ccda9f15d58ee376ab723fe23c13
d360 <50384721+dn-360@users.noreply.github.com>
2021-09-12 22:52:28 -0700

    Updated files for part 2

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by David Ng on 9/12/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
